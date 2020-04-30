b5c452eee95ceeb4510ba4dc3c768842e664fb55
Teemu Kolehmainen <teejuko@gmail.com>
2020-04-30 15:53:07 +0300

    hienosäätöä

--- src/main/java/swd/backend/plannerapp/PlannerappApplication.java
+++ src/main/java/swd/backend/plannerapp/PlannerappApplication.java
@@ -27,6 +27,9 @@ public class PlannerappApplication {
 			pRepository.save(new Plan("Siivoa", "Kotiaskareet", "imuroi ja tiskaa", "Tällä viikolla"));
 			pRepository.save(new Plan("Synttärit", "Kotiaskareet", "muista siivota ja leipoa kakku", "28.4."));
 			pRepository.save(new Plan("Afterparty", "Party", "muista kalja", "29.4."));
+			Plan plan = new Plan("Backend loppuprojekti", "Koulu/Työ", "Don't forget to deploy latest version to Heroku", "ASAP");
+			plan.setPriority(true);
+			pRepository.save(plan);
 			
 			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
 			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
